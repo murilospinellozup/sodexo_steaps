c82d8f5ce48d574aff06f27bd3b13c4af2741920
Murilo Spinello <murilo.spinello@zup.com.br>
2018-12-19 16:19:36 -0200

    fixes

--- pages/finish_order/finish_order.js
+++ pages/finish_order/finish_order.js
@@ -7,21 +7,53 @@ $("#barCode").html(barCode)
 $("#orderId").html(orderId)
 
 $("#copy").click(function(){
-
-	$("#t").show()
-	var t = document.getElementById('t')
-	t.innerHTML = barCode
-	t.select()
-
-	try {
-	  var successful = document.execCommand('copy')
-	  var msg = successful ? 'successfully' : 'unsuccessfully'
-	  console.log('text coppied ' + msg)
-	  alert("Código copiado!")
-	} catch (err) {
-	  console.log('Unable to copy text')
-	}
 	
-	t.innerHTML = ''
-	$("#t").hide()
+	Clipboard.copy(barCode);alert("Código copiado!")
+ 
 }) 
+
+window.Clipboard = (function(window, document, navigator) {
+	var textArea,
+			copy;
+
+	function isOS() {
+			return navigator.userAgent.match(/ipad|iphone/i);
+	}
+
+	function createTextArea(text) {
+			textArea = document.createElement('textArea');
+			textArea.value = text;
+			document.body.appendChild(textArea);
+	}
+
+	function selectText() {
+			var range,
+					selection;
+
+			if (isOS()) {
+					range = document.createRange();
+					range.selectNodeContents(textArea);
+					selection = window.getSelection();
+					selection.removeAllRanges();
+					selection.addRange(range);
+					textArea.setSelectionRange(0, 999999);
+			} else {
+					textArea.select();
+			}
+	}
+
+	function copyToClipboard() {        
+			document.execCommand('copy');
+			document.body.removeChild(textArea);
+	}
+
+	copy = function(text) {
+			createTextArea(text);
+			selectText();
+			copyToClipboard();
+	};
+
+	return {
+			copy: copy
+	};
+})(window, document, navigator);
